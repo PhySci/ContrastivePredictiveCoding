Bug fix of calculation graph and data generator.

train_cpc.py
@@ -3,7 +3,11 @@ from keras.layers import Dot, Lambda
 from keras.models import Model
 from keras.optimizers import SGD, adam
 from keras import backend as K
+from keras.backend import expand_dims
+from keras.callbacks import TensorBoard, ModelCheckpoint
 import tensorflow as tf
+import os
+import datetime
 
 try:
     from my_cpc.utils import ContrastiveDataGenerator, setup_logging
@@ -16,34 +20,35 @@ def get_encoder(x, emb_size):
     :param x:
     :return:
     """
-    with tf.name_scope('embedding_level_1'):
-        x = Conv1D(filters=10, strides=5, kernel_size=3)(x)
-        x = LeakyReLU()(x)
-        x = BatchNormalization()(x)
+    with tf.name_scope('Encoder'):
+        with tf.name_scope('embedding_level_1'):
+            x = Conv1D(filters=10, strides=5, kernel_size=3)(x)
+            x = LeakyReLU()(x)
+            x = BatchNormalization()(x)
 
-    with tf.name_scope('embedding_level_2'):
-        x = Conv1D(filters=8, strides=4, kernel_size=3)(x)
-        x = LeakyReLU()(x)
-        x = BatchNormalization()(x)
+        with tf.name_scope('embedding_level_2'):
+            x = Conv1D(filters=8, strides=4, kernel_size=3)(x)
+            x = LeakyReLU()(x)
+            x = BatchNormalization()(x)
 
-    with tf.name_scope('embedding_level_2'):
-        x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
-        x = LeakyReLU()(x)
-        x = BatchNormalization()(x)
+        with tf.name_scope('embedding_level_2'):
+            x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
+            x = LeakyReLU()(x)
+            x = BatchNormalization()(x)
 
-    with tf.name_scope('embedding_level_4'):
-        x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
-        x = LeakyReLU()(x)
-        x = BatchNormalization()(x)
+        with tf.name_scope('embedding_level_4'):
+            x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
+            x = LeakyReLU()(x)
+            x = BatchNormalization()(x)
 
-    with tf.name_scope('embedding_level_5'):
-        x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
-        x = LeakyReLU()(x)
-        x = BatchNormalization()(x)
+        with tf.name_scope('embedding_level_5'):
+            x = Conv1D(filters=4, strides=2, kernel_size=3)(x)
+            x = LeakyReLU()(x)
+            x = BatchNormalization()(x)
 
-    with tf.name_scope('embedding_dense'):
-        x = Flatten()(x)
-        x = Dense(units=emb_size, activation='relu')(x)
+        with tf.name_scope('embedding_dense'):
+            x = Flatten()(x)
+            x = Dense(units=emb_size, activation='relu')(x)
     return x
 
 
@@ -53,25 +58,26 @@ def network_autoregressive(x, code_size):
     :param x:
     :return:
     """
-    return GRU(units=code_size, return_sequences=False, name='ar_context')(x)
+    return GRU(units=code_size, return_sequences=False, name='autoregressive_context')(x)
 
 
 def loss_fn(y_true, y_pred):
     """
-    Contrstive loss function (eq. 4 from the original article)
+    Contrastive loss function (eq. 4 from the original article)
     # https://datascience.stackexchange.com/questions/25029/custom-loss-function-with-additional-parameter-in-keras
     :param y_true: labels (0, 1), where 0 means the sample was drawn from noisy distribution; 1 means the sample was
     drawn from the target distribution.
     :param y_pred: density ratio (f value from the original article)
     :return:
     """
-    divident = K.sum(K.dot(y_true, y_pred), axis=1)
-    divider = K.sum(y_pred, axis=1) + K.epsilon()
-    v = K.log(divident / divider)
-    return -v
+    with tf.name_scope('custom_loss_function'):
+        divident = K.sum(K.dot(y_true, y_pred), axis=1)
+        divider = K.sum(y_pred, axis=1) + K.epsilon()
+        l = -K.log(divident / divider)
+    return l*1e4
 
 
-def get_model(chunk_size, context_samples=100, contrastive_samples=10, emd_size=512):
+def get_model(chunk_size, context_samples=100, contrastive_samples=10, emd_size=512, gru_size=256):
     """
 
     :param chunk_size:
@@ -93,14 +99,17 @@ def get_model(chunk_size, context_samples=100, contrastive_samples=10, emd_size=
 
     # Workaround context
     x_encoded = TimeDistributed(encoder_model, name='Historical_embeddings')(x_input)
-    context = network_autoregressive(x_encoded, emd_size)
+    context = network_autoregressive(x_encoded, gru_size)
+    context = Lambda(lambda x: expand_dims(x, axis=-1), name='transpose_context')(context)
 
     # Make predictions for the next predict_terms timesteps
     z = TimeDistributed(encoder_model, name='Contrastive_embeddings')(y_input)
     # Equation 3
-    z1 = Lambda(lambda x: K.permute_dimensions(x, (0, 2, 1)), name='transpose')(z)
-    z2 = Dense(units=emd_size, name='W')(context)
-    f = Lambda(lambda x: K.exp(Dot(axes=1)(x)), name='multiplication')([z1, z2])
+    z2 = Dense(units=gru_size, name='W', use_bias=False)(z)
+    z2 = Lambda(lambda x: K.permute_dimensions(x, (0, 2, 1)), name='transpose')(z2)
+    d = Lambda(lambda x: Dot(axes=1)(x), name='multiplication')([z2, context])
+
+    f = Lambda(lambda x: K.exp(x), name='exponent')(d)
 
     # Model
     cpc_model = Model(inputs=[x_input, y_input], outputs=f) #, y_labels
@@ -109,6 +118,7 @@ def get_model(chunk_size, context_samples=100, contrastive_samples=10, emd_size=
 
 
 def train():
+    tmr = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
     # params
     K.set_learning_phase(1)
     chunk_size = 4096
@@ -116,6 +126,12 @@ def train():
     contrastive_samples = 1
     emd_size = 512
     batch_size = 16
+
+    params = {'model_name': 'cpc1'}
+    params.update({'checkpointer': {'verbose': 1,
+                                   'save_best_only': True,
+                                   'mode': 'min',
+                                    'monitor': 'loss'}})
 
     model_params = {'chunk_size': chunk_size,
                     'context_samples': context_samples,
@@ -135,13 +151,20 @@ def train():
                   'context_samples': context_samples,
                   'contrastive_samples': contrastive_samples}
 
+    output_folder = 'models'
+    tensorboard = TensorBoard(log_dir='./logs/' + 'cpc' + '_' + tmr,
+                              write_graph=True)
+    checkpointer = ModelCheckpoint(filepath=os.path.join(output_folder, params.get('model_name')+'.hdf5'),
+                                   **params['checkpointer'])
 
+    callbacks = [tensorboard, checkpointer]
     model = get_model(**model_params)
+
     # Compile model
-    model.compile(loss=loss_fn, optimizer=adam())
+    model.compile(loss=loss_fn, optimizer=adam(lr=1e-5))
 
     data_gen = ContrastiveDataGenerator(**gen_params)
-    model.fit_generator(generator=data_gen, epochs=10)
+    model.fit_generator(generator=data_gen, epochs=10, callbacks=callbacks, steps_per_epoch=30)
 
 if __name__=='__main__':
     setup_logging('train.log')

utils.py
@@ -9,112 +9,6 @@ import warnings
 from random import shuffle
 import numpy as np
 
-class SignalGenerator(Sequence):
-
-    def __init__(self, data_file='../input/train.parquet', meta_file='../input/metadata_train.csv',
-                 batch_size=10, shuffle=True, seed=42, test_mode=False, measurement_ids=None,
-                 normalize=True, compress_rate=1,
-                 wavelets=0, wavelet_params={}):
-        """
-        Constructor
-
-        :param data_file: path to data file
-        :param meta_file:  path to meta file
-        :param batch_size: batch size
-        :param measurement_ids: list of measurement ids. Dedicated for CV
-        :param shuffle:
-        :param seed: random seed
-        :param test_mode: return samples and signal_ids
-        :param normalize: to normalize the data
-        """
-        self.it = 0
-        self.shuffle = shuffle
-        self.data_pth = data_file
-        self.batch_size = batch_size
-        self.seed = seed
-        self.test_mode = test_mode
-
-        meta_df = pd.read_csv(meta_file)
-        if measurement_ids is not None:
-            meta_df = meta_df.query('id_measurement in @measurement_ids')
-
-        self.meta_df = meta_df.set_index('id_measurement', drop=True)
-        self.measurement_id_list = np.unique(self.meta_df.index.values)
-        self.list_sz = len(self.measurement_id_list)
-        self.max_it = int(np.ceil(self.list_sz / self.batch_size))
-        self.normalize = normalize
-        self.wavelets = wavelets
-        self.wavelet_params = wavelet_params
-
-        if 800000 % compress_rate !=0:
-            raise ValueError('Compress rate is not integer divider of signal length')
-        self.compress_rate = compress_rate
-
-    def __len__(self):
-        return self.max_it
-
-    def on_epoch_end(self):
-        """
-        Performs at the end of each epoch
-        :return:
-        """
-        shuffle(self.measurement_id_list)
-
-    def __getitem__(self, item):
-        """
-        Return one batch
-        :param item:
-        :return:
-        """
-        return self.__data_generation(item)
-
-    def __data_generation(self, it):
-        """
-        Data generator
-        :param it:
-        :return:
-        """
-        start = np.minimum(it * self.batch_size, self.list_sz)
-        end = np.minimum(start + self.batch_size, self.list_sz)
-        # list of measurements ids
-        measurement_id_batch = self.measurement_id_list[start:end]
-        # get list of meta info for the given measurements ids
-        data_batch = self.meta_df.loc[measurement_id_batch]
-
-        cols = data_batch.signal_id.astype(str).tolist()
-        data = pq.read_pandas(self.data_pth, columns=cols).to_pandas().values
-
-        if self.compress_rate > 1:
-            data = data.reshape([self.compress_rate, data.shape[0]//self.compress_rate, 3, -1], order='F')
-            data = data.sum(axis=0)
-        else:
-            data = data.reshape([data.shape[0], 3, -1], order='F')
-        data = np.moveaxis(data, -1, 0)
-
-        labels = data_batch.target.values.reshape([-1, 3])
-
-        if self.normalize:
-            data = data - data.mean(axis=(1, 2), keepdims=True)
-            data = data / data.std(axis=(1, 2), keepdims=True)
-
-        if self.wavelets == 1:
-            data = pywt.wavedec(data, axis=1, **self.wavelet_params)
-        elif self.wavelets ==2:
-            r = pywt.swt(data, axis=1, **self.wavelet_params)
-            ca = np.zeros([data.shape[0], data.shape[1], self.wavelet_params.get('level'), 3])
-            cd = np.zeros([data.shape[0], data.shape[1], self.wavelet_params.get('level'), 3])
-            for i, el in enumerate(r):
-                #ca[:, :, i, :] = el[0]
-                cd[:, :, i, :] = el[1]
-
-            #data = ca.reshape([data.shape[0], data.shape[1], -1], order='F')
-            data = cd.reshape([data.shape[0], data.shape[1], -1], order='F')
-
-        if self.test_mode:
-            return data, data_batch.signal_id
-        else:
-            return data, [labels[:, 0], labels[:, 1], labels[:, 2]]
-
 def setup_logging(fname, level=logging.DEBUG):
     """
     Create logger instance
@@ -224,6 +118,8 @@ class ContrastiveDataGenerator(Sequence):
         #idx = np.random.choice(range(self.batch_size), self.batch_size, replace=False)
         #contrastive_batch = contrastive_batch[idx, :, :]
         labels=np.zeros([self.batch_size, self.batch_size])
-        labels=np.ones([self.batch_size, self.batch_size])
+        labels=np.identity(self.batch_size)
+        labels = labels[:, :, np.newaxis]
         #labels[range(self.batch_size), idx] = 1
-        return ([context_batch[:, :, :, np.newaxis], contrastive_batch[:, :, :, np.newaxis]], labels)
+        s = ([context_batch[:, :, :, np.newaxis], contrastive_batch[:, :, :, np.newaxis]], labels)
+        return s